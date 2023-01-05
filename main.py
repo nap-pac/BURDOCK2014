4d47a367910f48723cb65ee26130644db5cc2b57
nap-pac <napoleonpaciente@yahoo.com>
2023-01-05 11:35:34 +0800

    iteration added

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #Imports
 import pyshark
 
+#input function
+def filt():
+    userInput = input("Ender Display filter: ")
+    return userInput
+
 #main function
-
 def main():
-    dp = input("Enter Display filer: ")
-    cap = pyshark.FileCapture('/home/philip/Documents/internship/app/pcaps/LANtest.pcap', display_filter=dp)
-    print(cap[0])
-
+    df = filt()
+    cap = pyshark.FileCapture('/home/philip/Documents/internship/app/pcaps/LANtest.pcap', display_filter=df)
+    for packet in cap:
+        print(packet.ip)
 
 #Calling Main
 main()
