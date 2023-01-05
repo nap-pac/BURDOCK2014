78d6e0ab06d0742b8430ae7f0e030d6c85f8395b
nap-pac <napoleonpaciente@yahoo.com>
2023-01-05 13:42:25 +0800

    figured out list within list

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 #Imports
 import pyshark
 
-#input function
+# FUNCTION: filt
+# ------------------
+# used for user input and input sanitisation
 def filt():
     userInput = input("Ender Display filter: ")
     return userInput
 
-#main function
+# MAIN
+# -------------
+# main function for the program
 def main():
+    #variables
+    i = 0
+    csv = []
+
     df = filt()
     cap = pyshark.FileCapture('/home/philip/Documents/internship/app/pcaps/LANtest.pcap', display_filter=df)
-    for packet in cap:
-        print(packet.ip)
+
+    srcIp = cap[0].ip.src
+    dstIp = cap[0].ip.dst
+    csv.append([srcIp, dstIp])
+    for a in csv:
+        print(a[0] + " and " + a[1])
+
+    #Make List of relevant information in order to later print in a .cvs format
+    #for packet in cap:
+        #srcIp = packet.ip.src
+        #dstIp = packet.ip.dst
+        #csv.append([srcIp, dstIp])
 
 #Calling Main
 main()
