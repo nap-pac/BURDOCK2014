603a7885bdf856e14b28ec31942147c0627d2d83
nap-pac <napoleonpaciente@yahoo.com>
2023-01-05 10:20:53 +0800

    main.py testing pyshark with pcap files

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello World")
+#Imports
+import pyshark
 
+#main function
+def main():
+    cap = pyshark.FileCapture('/home/philip/Documents/internship/app/pcaps/LANtest.pcap')
+    print(cap[0])
+
+
+#Calling Main
+main()
